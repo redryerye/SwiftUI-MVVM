c2be966915c858899431ab33e4c26655c0f54b3d
Yuki Yamamoto <yymmt3.waste@gmail.com>
2024-06-22 22:52:26 +0200

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Yuki Yamamoto on 2024/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
